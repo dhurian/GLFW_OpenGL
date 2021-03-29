1241271613565a0196624578bffc3030545489cc
Dhurian Vitoldas <dhurian@outlook.com>
2021-03-29 21:24:58 +0200

    Draw Triangle Branch

--- Source.cpp
+++ Source.cpp
@@ -35,10 +35,24 @@ int main() {
 
 	}
 	glViewport(0, 0, screenWidth, screenHeight);
+
+	float vertices[] = {
+		0.0, 0.5,0.0,
+		-0.5,-0.5,0.0,
+		0.5,-0.5,0.0
+	};
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, 0, vertices);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableClientState(GL_VERTEX_ARRAY);
+
+
+ 
 		glfwSwapBuffers(window);
 	}
 
