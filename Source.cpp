97da3973117940989d985f983410da6e257d838a
Dhurian Vitoldas <dhurian@outlook.com>
2021-04-02 18:32:10 +0200

    Finished

--- Source.cpp
+++ Source.cpp
@@ -37,14 +37,24 @@ int main() {
 		-0.5,-0.5,0.0,
 		0.5,-0.5,0.0
 	};
+
+	GLfloat colour[] = {
+	255,0,0,
+	0,255,0,
+	0,0,255
+	};
+
 	while (!glfwWindowShouldClose(window)) {
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_COLOR_ARRAY);
 		glVertexPointer(3, GL_FLOAT, 0, vertices);
+		glColorPointer(3, GL_FLOAT, 0, colour);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableClientState(GL_COLOR_ARRAY);
 		glDisableClientState(GL_VERTEX_ARRAY);
 
 
